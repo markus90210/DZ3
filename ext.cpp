22e5ea0caa133c4dd9206d2e2bbd691069d42bd7
Maxim <makc0890@mail.ru>
2021-02-28 23:54:08 +0300

    up

--- ext.cpp
+++ ext.cpp
@@ -1,14 +1,13 @@
-#include <iostream>
+
 
 int a, b, c, d;
  
-void insABCD(int *a,int *b,int *c,int *d){
-	puts("Vvedite chislo 'a': ");
-	std::cin >> *a;
-	puts("Vvedite chislo 'b': ");
-	std::cin >> *b;
-	puts("Vvedite chislo 'c': ");
-	std::cin >> *c;
-	puts("Vvedite chislo 'd': ");
-	std::cin >> *d;
+void insABCD(int *a, int *b, int *c, int *d){
+	*a = 11;
+	*b = 4;
+	*c = 5;
+	*d = 7;
 }
+	
+
+
--- main.cpp
+++ main.cpp
@@ -2,37 +2,45 @@
 
 
 
-//3-е задание
+// 3
 extern int a, b, c, d;
+void insABCD(int *a, int *b, int *c, int *d);
+const int g = 19;
 
-void insABCD(int *a,int *b,int *c,int *d);
 
-//2-e задание
-void insG(int*g){
-	std::cout << "Vvedite lyuboe celoe chislo: ";
-	std::cin >> *g;
-}
 int main(int argc, char *argv[]){
 
-	//1-е задание
-	puts(" Zadanie # 1(3)\n");
-	float df,  e;
-	std::cout << "Programma dlya vyvoda rezultata uravneniya 'a * (b + (c / d))'" << "\n" << std::endl;
+//	1
+
 	insABCD(&a, &b, &c, &d);
-	df = d;
-	e = (a * (b + (c / df)));
+	float df = d;
+	float e = a * (b + (c / df));
 	std::cout << e << std::endl;
+
+// 2
+
+	(g < 21) ? (std::cout <<  g - 21  << std::endl) : (std::cout <<  2 * (g - 21) << std::endl);
 	
-//	// 2-е задание
-//	puts(" Zadanie # 2\n");
-//	int g;
-//	insG(&g);
-//	(g < 21) ? (std::cout << "Raznica chisla " << g  << " i chisla 21 ravna: " << g - 21 << std::endl) : (std::cout << "Udvoenaya raznica chisla " << g << " i chisla 21 ravna: " << 2 * (g - 21) << std::endl);
-//	
-	//4-e задание
-	int arr3d[3][3][3];
+// 4
+	int arr3d[3][3][3], val = 0;	
+	for (int i = 0; i < 3; i++){
+		for (int j = 0; j < 3; j++){
+			for (int k = 0; k < 3; k++){
+				arr3d[i][j][k] = val;
+				val++;
+				std::cout << arr3d[i][j][k] << " " ;
+			}
+			std::cout << std::endl;
+		}
+		std::cout << std::endl;
+	}
+	int *parr3d = &arr3d[0][0][0];
+	int *p1arr3d = &arr3d[1][1][1];
+	std::cout << *p1arr3d << std::endl;
+
 	
 	
 	
 	return 0;
 }
+
